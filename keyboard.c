7b9e64daac96af4f17cf10cd0b587b7e664f961c
Datheprogramer <nguyenducdanhroblox@gmail.com>
2025-01-02 10:21:55 +0700

    Delete keyboard.c

--- keyboard.c
+++
@@ -1,9 +0,0 @@
-#include "keyboard.h"
-extern void print_string(const char *str);
-
-void init_keyboard(void) {}
-
-void print_char(char c) {
-    char str[2] = {c, '\0'};
-    print_string(str);
-}
